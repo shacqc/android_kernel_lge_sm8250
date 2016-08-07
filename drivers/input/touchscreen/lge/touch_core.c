5c2a97881eebd902e2923e82c9206efb92ece337
Louis Popi <theh2o64@gmail.com>
2016-08-07 09:18:52 +0200

    lge_touch: Send custom keycodes for supported gestures
    We'll handle these keycodes in userspace to make these
    off-screen gestures more useful.
    
    Signed-off-by: SGCMarkus <markusornik@gmail.com>

--- drivers/input/touchscreen/lge/touch_core.c
+++ drivers/input/touchscreen/lge/touch_core.c
@@ -34,6 +34,11 @@
 #include <touch_common.h>
 
 struct mutex touch_probe_lock;
+/* Gesture keycodes */
+#define KEY_GESTURE_SLIDE_UP	     249
+#define KEY_GESTURE_SLIDE_DOWN	     250
+#define KEY_GESTURE_SLIDE_RIGHT      251
+#define KEY_GESTURE_SLIDE_LEFT	     252
 
 u32 touch_debug_mask = BASE_INFO;
 /* Debug mask value
@@ -539,6 +544,14 @@ static int touch_init_input(struct touch_core_data *ts)
 	set_bit(EV_KEY, input->evbit);
 	set_bit(BTN_TOUCH, input->keybit);
 	set_bit(BTN_TOOL_FINGER, input->keybit);
+<<<<<<< HEAD
+=======
+	set_bit(KEY_WAKEUP, input->keybit);
+	set_bit(KEY_GESTURE_SLIDE_UP, input->keybit);
+	set_bit(KEY_GESTURE_SLIDE_DOWN, input->keybit);
+	set_bit(KEY_GESTURE_SLIDE_LEFT, input->keybit);
+	set_bit(KEY_GESTURE_SLIDE_RIGHT, input->keybit);
+>>>>>>> e9abc2cf2d9f (lge_touch: Send custom keycodes for supported gestures)
 	set_bit(INPUT_PROP_DIRECT, input->propbit);
 	input_set_abs_params(input, ABS_MT_POSITION_X, 0,
 			ts->caps.max_x, 0, 0);
@@ -808,6 +821,7 @@ void touch_send_uevent(struct touch_core_data *ts, int type)
 				KOBJ_CHANGE, uevent_str[type]);
 		TOUCH_I("%s\n",  uevent_str[type][0]);
 		touch_report_all_event(ts);
+<<<<<<< HEAD
 	} else {
 		reinit_completion(&ts->uevent_complete);
 
@@ -829,6 +843,48 @@ void touch_send_uevent(struct touch_core_data *ts, int type)
 		} else {
 			TOUCH_I("%s  is not sent\n", uevent_str[type][0]);
 		}
+=======
+	}
+	switch (type) {
+		case TOUCH_UEVENT_KNOCK:
+			input_report_key(ts->input, KEY_WAKEUP, 1);
+			TOUCH_I("Simulate power button depress\n");
+			input_sync(ts->input);
+			input_report_key(ts->input, KEY_WAKEUP, 0);
+			TOUCH_I("Simulate power button release\n");
+			input_sync(ts->input);
+			break;
+		case TOUCH_UEVENT_SWIPE_UP:
+			TOUCH_I("Swipe UP reported\n");
+			input_report_key(ts->input, KEY_GESTURE_SLIDE_UP, 1);
+			input_sync(ts->input);
+			input_report_key(ts->input, KEY_GESTURE_SLIDE_UP, 0);
+			input_sync(ts->input);
+			break;
+		case TOUCH_UEVENT_SWIPE_DOWN:
+			TOUCH_I("Swipe DOWN reported\n");
+			input_report_key(ts->input, KEY_GESTURE_SLIDE_DOWN, 1);
+			input_sync(ts->input);
+			input_report_key(ts->input, KEY_GESTURE_SLIDE_DOWN, 0);
+			input_sync(ts->input);
+			break;
+		case TOUCH_UEVENT_SWIPE_RIGHT:
+			TOUCH_I("Swipe RIGHT reported\n");
+			input_report_key(ts->input, KEY_GESTURE_SLIDE_RIGHT, 1);
+			input_sync(ts->input);
+			input_report_key(ts->input, KEY_GESTURE_SLIDE_RIGHT, 0);
+			input_sync(ts->input);
+			break;
+		case TOUCH_UEVENT_SWIPE_LEFT:
+			TOUCH_I("Swipe LEFT reported\n");
+			input_report_key(ts->input, KEY_GESTURE_SLIDE_LEFT, 1);
+			input_sync(ts->input);
+			input_report_key(ts->input, KEY_GESTURE_SLIDE_LEFT, 0);
+			input_sync(ts->input);
+			break;
+		default:
+			break;
+>>>>>>> e9abc2cf2d9f (lge_touch: Send custom keycodes for supported gestures)
 	}
 }
 
